7aba4ec4618a8552eee2c034270d3f8a23c582b4
Ryan Olden <103864719+jabree937@users.noreply.github.com>
2022-10-31 23:34:36 -0400

    Add files via upload

---
+++ BestSellerBook.kt
@@ -0,0 +1,23 @@
+package com.codepath.project3api
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class BestSellerBook {
+    @SerializedName("rank")
+    var rank = 0
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+    @JvmField
+    @SerializedName("author")
+    var author: String? = null
+
+}
